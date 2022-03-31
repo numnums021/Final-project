65a098bb564b2273e9db3bba05752789f7954386
danya <numnums021@gmail.com>
2022-03-31 17:33:18 +0300

    Добавлено взаимодействие клиента с сервером

--- client/src/main/java/ru/hj77/client/dto/BalanceDTO.java
+++ client/src/main/java/ru/hj77/client/dto/BalanceDTO.java
@@ -3,11 +3,13 @@ package ru.hj77.client.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Getter
+@ToString
 public class BalanceDTO {
     private double balance;
 }
